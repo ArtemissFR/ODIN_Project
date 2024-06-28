3a6bc3b662febc75f3d547038f3494b8c733db0d
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-06-28 15:38:14 +0200

    Update Hello_My_Dir.ps1

--- Hello-My-Dir/Hello_My_Dir.ps1
+++ Hello-My-Dir/Hello_My_Dir.ps1
@@ -30,12 +30,12 @@ Remove-Item -Path $zipPath
 Write-Host "Fichier zip supprimé."
 
 # Copie le fichier RunSetup.xml dans le dossier décompressé
-$sourcePath = "Configuration\RunSetup.xml"
-$destinationPath = "C:\ODIN\Hello-My-Dir\Hello-My-Dir-main\Configuration\RunSetup.xml"
+#$sourcePath = "Configuration\RunSetup.xml"
+#$destinationPath = "C:\ODIN\Hello-My-Dir\Hello-My-Dir-main\Configuration\RunSetup.xml"
 
-if (-Not (Test-Path -Path $destinationPath)) {
-    Copy-Item -Path $sourcePath -Destination $destinationPath
-    Write-Host "Fichier RunSetup.xml copié avec succès."
-} else {
-    Write-Host "Le fichier RunSetup.xml existe déjà dans le dossier destination."
-}
+#if (-Not (Test-Path -Path $destinationPath)) {
+#    Copy-Item -Path $sourcePath -Destination $destinationPath
+#    Write-Host "Fichier RunSetup.xml copié avec succès."
+#} else {
+#    Write-Host "Le fichier RunSetup.xml existe déjà dans le dossier destination."
+#}
