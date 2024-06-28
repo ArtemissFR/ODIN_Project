8da06591f04892e58e675322180bbefb91fcbcd2
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-06-28 14:07:06 +0200

    Create Hello_My_Dir.ps1

---
+++ Hello-My-Dir/Hello_My_Dir.ps1
@@ -0,0 +1,30 @@
+# Vérifie si le dossier C:\ODIN existe, sinon le créer
+if (-Not (Test-Path -Path "C:\ODIN")) {
+    New-Item -Path "C:\ODIN" -ItemType Directory
+    Write-Host "Dossier C:\ODIN créé."
+} else {
+    Write-Host "Dossier C:\ODIN existe déjà."
+}
+
+# Vérifie si le dossier C:\ODIN\Hello-My-Dir existe, sinon le créer
+if (-Not (Test-Path -Path "C:\ODIN\Hello-My-Dir")) {
+    New-Item -Path "C:\ODIN\Hello-My-Dir" -ItemType Directory
+    Write-Host "Dossier C:\ODIN\Hello-My-Dir créé."
+} else {
+    Write-Host "Dossier C:\ODIN\Hello-My-Dir existe déjà."
+}
+
+# Télécharge le fichier zip depuis GitHub
+$zipUrl = "https://github.com/LoicVeirman/Hello-My-Dir/archive/refs/heads/main.zip"
+$zipPath = "C:\ODIN\Hello-My-Dir\Hello-My-Dir.zip"
+
+Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
+Write-Host "Téléchargement de l'archive GitHub terminé."
+
+# Décompresse le fichier zip téléchargé
+Expand-Archive -Path $zipPath -DestinationPath "C:\ODIN\Hello-My-Dir"
+Write-Host "Décompression de l'archive terminée."
+
+# Supprime le fichier zip après décompression
+Remove-Item -Path $zipPath
+Write-Host "Fichier zip supprimé."
