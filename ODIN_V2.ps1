1d7b3c4283d2c43148dd065e5feb5ed0a3b12f40
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-06-28 16:07:47 +0200

    Create ODIN_V2.ps1

---
+++ ODIN_V2.ps1
@@ -0,0 +1,57 @@
+# Chargement des Assemblies nécessaires
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+# Chemin vers les fichiers que tu veux débloquer
+$scripts = @(
+    "PingCastle\PingCastle.ps1",
+    "RansomLord\RansomLord_v3.1.ps1",
+    "Hello-My-Dir\Hello_My_Dir.ps1",
+    "Hello-My-Dir\Hello_My_Dir_Config.ps1"
+    # Ajoute d'autres scripts si nécessaire
+)
+
+# Débloquer tous les scripts
+$scripts | ForEach-Object {
+    Unblock-File -Path $_
+}
+
+# Création de la Forme
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Choix d'Options"
+$form.Size = New-Object System.Drawing.Size(300, 200)
+$form.StartPosition = "CenterScreen"
+$form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog
+$form.MaximizeBox = $false
+$form.MinimizeBox = $false
+
+# Création du bouton de lancement
+$buttonLaunch = New-Object System.Windows.Forms.Button
+$buttonLaunch.Location = New-Object System.Drawing.Point(50, 50)
+$buttonLaunch.Size = New-Object System.Drawing.Size(200, 30)
+$buttonLaunch.Text = "Démarrer les scripts"
+$buttonLaunch.Add_Click({
+    # Lancer les scripts un par un dans l'ordre
+    foreach ($script in $scripts) {
+        Start-Process powershell -ArgumentList "-File $script"
+        # Attendre que le script actuel se termine avant de continuer
+        Start-Sleep -Seconds 5 # Temps d'attente ajustable
+    }
+    $form.Close()
+})
+
+# Création du bouton d'annulation
+$buttonCancel = New-Object System.Windows.Forms.Button
+$buttonCancel.Location = New-Object System.Drawing.Point(50, 100)
+$buttonCancel.Size = New-Object System.Drawing.Size(200, 30)
+$buttonCancel.Text = "Annuler"
+$buttonCancel.Add_Click({
+    $form.Close()
+})
+
+# Ajout des contrôles à la Forme
+$form.Controls.Add($buttonLaunch)
+$form.Controls.Add($buttonCancel)
+
+# Affichage de la Forme
+$form.ShowDialog()
