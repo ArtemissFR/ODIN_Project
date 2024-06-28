4fd2baf230f6c402e6cd127f53ac797bd9fb0424
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-06-28 12:54:26 +0200

    Update RansomLord_v3.1.ps1

--- RansomLord/RansomLord_v3.1.ps1
+++ RansomLord/RansomLord_v3.1.ps1
@@ -1,17 +1,22 @@
 # Définir l'URL de téléchargement et l'emplacement du fichier
 $url = "https://github.com/malvuln/RansomLord/releases/download/v3/RansomLord_v3.1.exe"
-$destinationPath = "C:\RansomLord\RansomLord_v3.1.exe"
+$destinationPath = "C:\ODIN\RansomLord\RansomLord_v3.1.exe"
 
 # Créer le dossier si il n'existe pas
-if (-Not (Test-Path "C:\RansomLord")) {
-    New-Item -ItemType Directory -Path "C:\RansomLord"
+if (-Not (Test-Path "C:\ODIN")) {
+    New-Item -ItemType Directory -Path "C:\ODIN"
+}
+
+# Créer le dossier si il n'existe pas
+if (-Not (Test-Path "C:\ODIN\RansomLord")) {
+    New-Item -ItemType Directory -Path "C:\ODIN\RansomLord"
 }
 
 # Télécharger le fichier
 Invoke-WebRequest -Uri $url -OutFile $destinationPath
 
 # Changer de répertoire
-Set-Location "C:\RansomLord"
+Set-Location "C:\ODIN\RansomLord"
 
 # Exécuter le fichier avec les paramètres
 Start-Process -FilePath ".\RansomLord_v3.1.exe" -ArgumentList "-g" -Wait
