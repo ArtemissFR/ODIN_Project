a9c346d786693019026f6b889c41574ee68048bd
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-06-28 15:37:51 +0200

    Create Hello_My_Dir_Config.ps1

---
+++ Hello-My-Dir/Hello_My_Dir_Config.ps1
@@ -0,0 +1,10 @@
+# Copie le fichier RunSetup.xml dans le dossier décompressé
+$sourcePath = "Configuration\RunSetup.xml"
+$destinationPath = "C:\ODIN\Hello-My-Dir\Hello-My-Dir-main\Configuration\RunSetup.xml"
+
+if (-Not (Test-Path -Path $destinationPath)) {
+    Copy-Item -Path $sourcePath -Destination $destinationPath
+    Write-Host "Fichier RunSetup.xml copié avec succès."
+} else {
+    Write-Host "Le fichier RunSetup.xml existe déjà dans le dossier destination."
+}
