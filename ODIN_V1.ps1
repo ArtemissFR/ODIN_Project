3a1035f67176367cb361ff0ffbfa635f76f68113
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-06-28 16:19:21 +0200

    Update ODIN_V1.ps1

--- ODIN_V1.ps1
+++ ODIN_V1.ps1
@@ -21,7 +21,7 @@ Unblock-File -Path $scriptODIN
 # Création de la Forme
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "Choix d'Options"
-$form.Size = New-Object System.Drawing.Size(300,300)
+$form.Size = New-Object System.Drawing.Size(300,500)
 $form.StartPosition = "CenterScreen"
 $form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog
 $form.MaximizeBox = $false
@@ -31,26 +31,8 @@ $form.MinimizeBox = $false
 $button1 = New-Object System.Windows.Forms.Button
 $button1.Location = New-Object System.Drawing.Point(50,50)
 $button1.Size = New-Object System.Drawing.Size(200,30)
-$button1.Text = "PingCastle"
+$button1.Text = "Hello-My-Dir"
 $button1.Add_Click({
-    Start-Process powershell -ArgumentList "-File PingCastle\PingCastle.ps1"
-    $form.Close()
-})
-
-$button2 = New-Object System.Windows.Forms.Button
-$button2.Location = New-Object System.Drawing.Point(50,90)
-$button2.Size = New-Object System.Drawing.Size(200,30)
-$button2.Text = "RansomLord"
-$button2.Add_Click({
-    Start-Process powershell -ArgumentList "-File RansomLord\RansomLord_v3.1.ps1"
-    $form.Close()
-})
-
-$button3 = New-Object System.Windows.Forms.Button
-$button3.Location = New-Object System.Drawing.Point(50,130)
-$button3.Size = New-Object System.Drawing.Size(200,30)
-$button3.Text = "Hello-My-Dir"
-$button3.Add_Click({
     Start-Process powershell -ArgumentList "-File Hello-My-Dir\Hello_My_Dir.ps1"
     # Attendre que le premier script se termine avant d'exécuter le second script
     Start-Sleep -Seconds 30 # Ajuste le temps d'attente si nécessaire
@@ -58,17 +40,44 @@ $button3.Add_Click({
     $form.Close()
 })
 
+$button2 = New-Object System.Windows.Forms.Button
+$button2.Location = New-Object System.Drawing.Point(50,90)
+$button2.Size = New-Object System.Drawing.Size(200,30)
+$button2.Text = "Harden-AD (in progress ...)"
+$button2.Add_Click({
+    Start-Process powershell -ArgumentList "-File Harden-AD\Harden-AD.ps1"
+    $form.Close()
+})
+
+$button3 = New-Object System.Windows.Forms.Button
+$button3.Location = New-Object System.Drawing.Point(50,130)
+$button3.Size = New-Object System.Drawing.Size(200,30)
+$button3.Text = "RansomLord"
+$button3.Add_Click({
+    Start-Process powershell -ArgumentList "-File RansomLord\RansomLord_v3.1.ps1"
+    $form.Close()
+})
+
 $button4 = New-Object System.Windows.Forms.Button
 $button4.Location = New-Object System.Drawing.Point(50,170)
 $button4.Size = New-Object System.Drawing.Size(200,30)
-$button4.Text = "HardenAD (in progress...)"
+$button4.Text = "PingCastle"
 $button4.Add_Click({
-    Start-Process powershell -ArgumentList "-File Harden-AD\HardenAD.ps1"
+    Start-Process powershell -ArgumentList "-File PingCastle\PingCastle.ps1"
+    $form.Close()
+})
+
+$button5 = New-Object System.Windows.Forms.Button
+$button5.Location = New-Object System.Drawing.Point(50,210)
+$button5.Size = New-Object System.Drawing.Size(200,30)
+$button5.Text = "PurpleKnight"
+$button5.Add_Click({
+    Start-Process powershell -ArgumentList "-File PurpleKnight\PurpleKnight.ps1"
     $form.Close()
 })
 
 $buttonCancel = New-Object System.Windows.Forms.Button
-$buttonCancel.Location = New-Object System.Drawing.Point(50,210)
+$buttonCancel.Location = New-Object System.Drawing.Point(50,250)
 $buttonCancel.Size = New-Object System.Drawing.Size(200,30)
 $buttonCancel.Text = "Annuler"
 $buttonCancel.Add_Click({
