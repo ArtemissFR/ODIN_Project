2a3f6ee3053bfce4b5a87b93159490b15d566073
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-07-01 15:21:36 +0200

    Update ADACLScanner.ps1

--- ADACLScanner/ADACLScanner.ps1
+++ ADACLScanner/ADACLScanner.ps1
@@ -1,1 +1,31 @@
+# Vérifie si le dossier C:\ODIN existe, sinon le créer
+if (-Not (Test-Path -Path "C:\ODIN")) {
+    New-Item -Path "C:\ODIN" -ItemType Directory
+    Write-Host "Dossier C:\ODIN créé."
+} else {
+    Write-Host "Dossier C:\ODIN existe déjà."
+}
+
+# Vérifie si le dossier C:\ODIN\Hello-My-Dir existe, sinon le créer
+if (-Not (Test-Path -Path "C:\ODIN\AD ACL Scanner")) {
+    New-Item -Path "C:\ODIN\AD ACL Scanner" -ItemType Directory
+    Write-Host "Dossier C:\ODIN\AD ACL Scanner créé."
+} else {
+    Write-Host "Dossier C:\ODIN\AD ACL Scanner existe déjà."
+}
+
+# Télécharge le fichier zip depuis GitHub
+$zipUrl = "https://github.com/LoicVeirman/HardenAD/archive/refs/heads/Master.zip"
+$zipPath = "C:\ODIN\AD ACL Scanner\AD-ACL-Scanner.zip"
+
+Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
+Write-Host "Téléchargement de l'archive GitHub terminé."
+
+# Décompresse le fichier zip téléchargé
+Expand-Archive -Path $zipPath -DestinationPath "C:\ODIN\AD ACL Scanner"
+Write-Host "Décompression de l'archive terminée."
+
+# Supprime le fichier zip après décompression
+Remove-Item -Path $zipPath
+Write-Host "Fichier zip supprimé."
 
