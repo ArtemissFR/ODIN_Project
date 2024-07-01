65991c9351fd9087a7e5cb9a1c8c9bc2ec544765
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-07-01 15:35:12 +0200

    Update ODIN_V1.0.ps1

--- ODIN_V1.0.ps1
+++ ODIN_V1.0.ps1
@@ -3,26 +3,26 @@ Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
 # Path to the files you want to unlock
-$scriptHelloMyDir = "Hello-My-Dir\Hello_My_Dir.ps1"
-$scriptHelloMyDirConfig = "Hello-My-Dir\Hello_My_Dir_Config.ps1"
-$scriptHardenAD = "Harden-AD\Harden-AD.ps1"
-$scriptPingCastle = "PingCastle\PingCastle.ps1"
-$scriptPurpleKnight = "PurpleKnight\PurpleKnight.ps1"
-$scriptRansomLord = "RansomLord\RansomLord_v3.1.ps1"
-$scriptADACLScanner = "ADACLScanner\ADACLScanner.ps1"
-$scriptODIN = "ODIN_V1.0.ps1"
-$scriptODINstart = "ODIN_start_V1.0.ps1"
+$scriptHelloMyDir = "Hello-My-Dir\Hello_My_Dir.ps1" # --> Hello-My-Dir
+$scriptHelloMyDirConfig = "Hello-My-Dir\Hello_My_Dir_Config.ps1" # --> Hello-My-Dir Configuration
+$scriptHardenAD = "Harden-AD\Harden-AD.ps1" # --> HardenAD
+$scriptPingCastle = "PingCastle\PingCastle.ps1" # --> PingCastle
+$scriptPurpleKnight = "PurpleKnight\PurpleKnight.ps1" # --> PurpleKnight
+$scriptRansomLord = "RansomLord\RansomLord_v3.1.ps1" # --> RansomLord
+$scriptADACLScanner = "ADACLScanner\ADACLScanner.ps1" # --> AD ACL Scanner
+$scriptODIN = "ODIN_V1.0.ps1" # --> ODIN Installer
+$scriptODINstart = "ODIN_start_V1.0.ps1" # --> ODIN Gui application launcher
 
-# Use of cmdlet Unblock-File
-Unblock-File -Path $scriptHelloMyDir # Unblock script powershell for : Hello-My-Dir
-Unblock-File -Path $scriptHelloMyDirConfig # Unblock script powershell for : Hello-My-Dir Configuration
-Unblock-File -Path $scriptHardenAD # Unblock script powershell for : HardenAD
-Unblock-File -Path $scriptPingCastle # Unblock script powershell for : PingCastle
-Unblock-File -Path $scriptPurpleKnight # Unblock script powershell for : PurpleKnight
-Unblock-File -Path $scriptRansomLord # Unblock script powershell for : RansomLord
-Unblock-File -Path $scriptADACLScanner # Unblock script powershell for : AD ACL Scanner
-Unblock-File -Path $scriptODIN # Unblock script powershell for : ODIN Installer
-Unblock-File -Path $scriptODINstart # Unblock script powershell for : ODIN Gui application launcher
+# Use of cmdlet Unblock-File --> Unblock script powershell
+Unblock-File -Path $scriptHelloMyDir # --> Hello-My-Dir
+Unblock-File -Path $scriptHelloMyDirConfig # --> Hello-My-Dir Configuration
+Unblock-File -Path $scriptHardenAD # --> HardenAD
+Unblock-File -Path $scriptPingCastle # --> PingCastle
+Unblock-File -Path $scriptPurpleKnight # --> PurpleKnight
+Unblock-File -Path $scriptRansomLord # --> RansomLord
+Unblock-File -Path $scriptADACLScanner # --> AD ACL Scanner
+Unblock-File -Path $scriptODIN # --> ODIN Installer
+Unblock-File -Path $scriptODINstart # --> ODIN Gui application launcher
 
 # Creating the Form
 $form = New-Object System.Windows.Forms.Form
@@ -80,18 +80,18 @@ $button5.Add_Click({
 $buttonCancel = New-Object System.Windows.Forms.Button
 $buttonCancel.Location = New-Object System.Drawing.Point(50,250)
 $buttonCancel.Size = New-Object System.Drawing.Size(200,30)
-$buttonCancel.Text = "Terminer"
+$buttonCancel.Text = "Finish"
 $buttonCancel.Add_Click({
     $form.Close()
 })
 
 # Ajout des contrôles à la Forme
-$form.Controls.Add($button1) # Hello-My-Dir
-$form.Controls.Add($button2) # Harden-AD
-$form.Controls.Add($button3) # RansomLord
-$form.Controls.Add($button4) # PingCastle
-$form.Controls.Add($button5) # PurpleKnight
-$form.Controls.Add($buttonCancel) # Terminer
+$form.Controls.Add($button1) # --> Hello-My-Dir
+$form.Controls.Add($button2) # --> Harden-AD
+$form.Controls.Add($button3) # --> RansomLord
+$form.Controls.Add($button4) # --> PingCastle
+$form.Controls.Add($button5) # --> PurpleKnight
+$form.Controls.Add($buttonCancel) # --> Finish
 
 # Affichage de la Forme
 $form.ShowDialog()
