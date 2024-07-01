39be13d2264517d5a1ba11c83aed5adb6ae0e6cb
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-07-01 16:53:28 +0200

    Update ODIN_Executer_V1.0.ps1

--- ODIN_Executer_V1.0.ps1
+++ ODIN_Executer_V1.0.ps1
@@ -1,16 +1,16 @@
-# Chargement des Assemblies nécessaires
+# Loading the necessary Assemblies
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
-# Chemin vers les fichiers que tu veux débloquer
+# Path to the files you want to unlock
 $scriptHardenADStart = "Harden-AD\Harden-AD_start.ps1"
 $scriptADACLScanner = "AD ACL Scanner\ADACLScan.ps1"
 
-# Utilisation de la cmdlet Unblock-File
+# Use of cmdlet Unblock-File --> Unblock script powershell
 Unblock-File -Path $scriptHardenADStart
 Unblock-File -Path $scriptADACLScanner
 
-# Création de la Forme
+# Creating the Form
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "[ODIN] - APPLICATION LAUNCHER"
 $form.Size = New-Object System.Drawing.Size(300,400)
@@ -19,7 +19,7 @@ $form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog
 $form.MaximizeBox = $false
 $form.MinimizeBox = $false
 
-# Création des Boutons
+# BUTTON 1 --> HELLO-MY-DIR
 $button1 = New-Object System.Windows.Forms.Button
 $button1.Location = New-Object System.Drawing.Point(50,50)
 $button1.Size = New-Object System.Drawing.Size(200,30)
@@ -30,7 +30,9 @@ $button1.Add_Click({
     Start-Sleep -Seconds 30 # Ajuste le temps d'attente si nécessaire
     Start-Process powershell -ArgumentList "-File Hello-My-Dir\Hello_My_Dir_Config.ps1"
 })
+# BUTTON 1 --> HELLO-MY-DIR
 
+# BUTTON 2 --> HARDEN-AD
 $button2 = New-Object System.Windows.Forms.Button
 $button2.Location = New-Object System.Drawing.Point(50,90)
 $button2.Size = New-Object System.Drawing.Size(200,30)
@@ -38,7 +40,9 @@ $button2.Text = "Execute Harden-AD"
 $button2.Add_Click({
     Start-Process powershell -ArgumentList "-File Harden-AD\Harden-AD_start.ps1"
 })
+# BUTTON 2 --> HARDEN-AD
 
+# BUTTON 3 --> PINGCASTLE
 $button3 = New-Object System.Windows.Forms.Button
 $button3.Location = New-Object System.Drawing.Point(50,130)
 $button3.Size = New-Object System.Drawing.Size(200,30)
@@ -47,7 +51,9 @@ $button3.Add_Click({
     $pingcastle_path = "C:\ODIN\PingCastle"
     Start-Process powershell -ArgumentList "-NoProfile -NoExit -Command `"Set-Location -Path $pingcastle_path; .\PingCastle.exe`""
 })
+# BUTTON 3 --> PINGCASTLE
 
+# BUTTON 4 --> PURPLEKNIGHT
 $button4 = New-Object System.Windows.Forms.Button
 $button4.Location = New-Object System.Drawing.Point(50,170)
 $button4.Size = New-Object System.Drawing.Size(200,30)
@@ -56,7 +62,9 @@ $button4.Add_Click({
     $purpleknight_path = "C:\ODIN\PurpleKnight\PK Community 4.2"
     Start-Process powershell -ArgumentList "-NoProfile -NoExit -Command `"Set-Location -Path $purpleknight_path; .\PurpleKnight.exe`""
 })
+# BUTTON 4 --> PURPLEKNIGHT
 
+# BUTTON 5 --> AD ACL Scanner
 $button5 = New-Object System.Windows.Forms.Button
 $button5.Location = New-Object System.Drawing.Point(50,210)
 $button5.Size = New-Object System.Drawing.Size(200,30)
@@ -65,7 +73,9 @@ $button5.Add_Click({
     $adaclscanner_path = "C:\ODIN\AD ACL Scanner\ADACLScanner-master"
     Start-Process powershell -ArgumentList "-NoProfile -NoExit -Command `"Set-Location -Path $adaclscanner_path; .\ADACLScan.ps1`""
 })
+# BUTTON 5 --> AD ACL Scanner
 
+# BUTTON 6 --> FINISH
 $buttonCancel = New-Object System.Windows.Forms.Button
 $buttonCancel.Location = New-Object System.Drawing.Point(50,250)
 $buttonCancel.Size = New-Object System.Drawing.Size(200,30)
@@ -73,6 +83,7 @@ $buttonCancel.Text = "Finish"
 $buttonCancel.Add_Click({
     $form.Close()
 })
+# BUTTON 6 --> FINISH
 
 # Ajout des contrôles à la Forme
 $form.Controls.Add($button1) # Hello-My-Dir
