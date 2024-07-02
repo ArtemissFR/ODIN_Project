7cd9bcc0e00ae89f7d3203a85e0742d7c90d5c07
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-07-02 15:14:39 +0200

    Update ODIN_V3.0.ps1

--- ODIN_V3.0.ps1
+++ ODIN_V3.0.ps1
@@ -37,15 +37,80 @@ function Create-FormInstall {
     $label.Location = New-Object System.Drawing.Point(10,10)
     $form.Controls.Add($label)
 
-    $nextButton = New-Object System.Windows.Forms.Button
-    $nextButton.Text = "Next"
-    $nextButton.Location = New-Object System.Drawing.Point(200,130)
-    $nextButton.Add_Click({ 
+    # BUTTON 1 --> HELLO-MY-DIR
+    $button1_hellomydir = New-Object System.Windows.Forms.Button
+    $button1_hellomydir.Location = New-Object System.Drawing.Point(50,50)
+    $button1_hellomydir.Size = New-Object System.Drawing.Size(200,30)
+    $button1_hellomydir.Text = "Hello-My-Dir"
+    $button1_hellomydir.Add_Click({
+        Start-Process powershell -ArgumentList "-File Hello-My-Dir\Hello_My_Dir.ps1"
+        # Attendre que le premier script se termine avant d'exécuter le second script
+        Start-Sleep -Seconds 30 # Ajuste le temps d'attente si nécessaire
+        Start-Process powershell -ArgumentList "-File Hello-My-Dir\Hello_My_Dir_Config.ps1"
+    })
+    # BUTTON 1 --> HELLO-MY-DIR
+    
+    # BUTTON 2 --> HARDEN-AD
+    $button2_hardenad = New-Object System.Windows.Forms.Button
+    $button2_hardenad.Location = New-Object System.Drawing.Point(50,90)
+    $button2_hardenad.Size = New-Object System.Drawing.Size(200,30)
+    $button2_hardenad.Text = "Harden-AD (in progress ...)"
+    $button2_hardenad.Add_Click({
+        Start-Process powershell -ArgumentList "-File Harden-AD\Harden-AD.ps1"
+    })
+    # BUTTON 2 --> HARDEN-AD
+    
+    # BUTTON 3 --> RANSOMLORD
+    $button3_ransomlord = New-Object System.Windows.Forms.Button
+    $button3_ransomlord.Location = New-Object System.Drawing.Point(50,130)
+    $button3_ransomlord.Size = New-Object System.Drawing.Size(200,30)
+    $button3_ransomlord.Text = "RansomLord"
+    $button3_ransomlord.Add_Click({
+        Start-Process powershell -ArgumentList "-File RansomLord\RansomLord_v3.1.ps1"
+    })
+    # BUTTON 3 --> RANSOMLORD
+    
+    # BUTTON 4 --> PINGCASTLE
+    $button4_pingcastle = New-Object System.Windows.Forms.Button
+    $button4_pingcastle.Location = New-Object System.Drawing.Point(50,170)
+    $button4_pingcastle.Size = New-Object System.Drawing.Size(200,30)
+    $button4_pingcastle.Text = "PingCastle"
+    $button4_pingcastle.Add_Click({
+        Start-Process powershell -ArgumentList "-File PingCastle\PingCastle.ps1"
+    })
+    # BUTTON 4 --> PINGCASTLE
+    
+    # BUTTON 5 --> PURPLEKNIGHT
+    $button5_purpleknight = New-Object System.Windows.Forms.Button
+    $button5_purpleknight.Location = New-Object System.Drawing.Point(50,210)
+    $button5_purpleknight.Size = New-Object System.Drawing.Size(200,30)
+    $button5_purpleknight.Text = "PurpleKnight"
+    $button5_purpleknight.Add_Click({
+        Start-Process powershell -ArgumentList "-File PurpleKnight\PurpleKnight.ps1"
+    })
+    # BUTTON 5 --> PURPLEKNIGHT
+    
+    # BUTTON 6 --> AD ACL Scanner
+    $button6_adaclscanner = New-Object System.Windows.Forms.Button
+    $button6_adaclscanner.Location = New-Object System.Drawing.Point(50,250)
+    $button6_adaclscanner.Size = New-Object System.Drawing.Size(200,30)
+    $button6_adaclscanner.Text = "AD ACL Scanner"
+    $button6_adaclscanner.Add_Click({
+        Start-Process powershell -ArgumentList "-File ADACLScanner\ADACLScanner.ps1"
+    })
+    # BUTTON 6 --> AD ACL Scanner
+
+    # BUTTON 7 --> Next
+    $button7_next = New-Object System.Windows.Forms.Button
+    $button7_next.Text = "Next"
+    $button7_next.Location = New-Object System.Drawing.Point(200,130)
+    $button7_next.Add_Click({ 
         $form.Hide()
         Create-FormLaunch
         $form.Close()
     })
-    $form.Controls.Add($nextButton)
+    # BUTTON 7 --> Next   
+    $form.Controls.Add($button7_next)
 
     $form.ShowDialog()
 }
