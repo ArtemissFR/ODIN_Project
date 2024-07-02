a85216759576c58864e1dd94fb3784328fd0f6a9
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-07-02 15:29:38 +0200

    Update ODIN_V3.0.ps1

--- ODIN_V3.0.ps1
+++ ODIN_V3.0.ps1
@@ -41,7 +41,7 @@ Unblock-File -Path $scriptODINExecuter # --> ODIN Gui application launcher
 function Create-FormInstall {
     $form = New-Object System.Windows.Forms.Form
     $form.Text = "[ODIN] - DOWNLOADER / INSTALLER"
-    $form.Size = New-Object System.Drawing.Size(300,500)
+    $form.Size = New-Object System.Drawing.Size(300,450)
     $form.StartPosition = "CenterScreen"
     $form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog
     $form.MaximizeBox = $false
@@ -49,7 +49,7 @@ function Create-FormInstall {
 
     $label = New-Object System.Windows.Forms.Label
     $label.Text = "[ODIN] - DOWNLOADER / INSTALLER"
-    $label.Location = New-Object System.Drawing.Point(10,10)
+ #   $label.Location = New-Object System.Drawing.Point(10,10)
     $form.Controls.Add($label)
 
     # BUTTON 1 --> HELLO-MY-DIR
@@ -151,16 +151,82 @@ function Create-FormLaunch {
 
     $label = New-Object System.Windows.Forms.Label
     $label.Text = "[ODIN] - APPLICATION LAUNCHER"
-    $label.Location = New-Object System.Drawing.Point(10,10)
+#    $label.Location = New-Object System.Drawing.Point(10,10)
     $form.Controls.Add($label)
 
-    $launchButton = New-Object System.Windows.Forms.Button
-    $launchButton.Text = "Launch"
-    $launchButton.Location = New-Object System.Drawing.Point(200,130)
-    $launchButton.Add_Click({
-#        [System.Windows.Forms.MessageBox]::Show(" !")
+    # BUTTON 1 --> HELLO-MY-DIR
+    $button1_exe_hellomydir = New-Object System.Windows.Forms.Button
+    $button1_exe_hellomydir.Location = New-Object System.Drawing.Point(50,50)
+    $button1_exe_hellomydir.Size = New-Object System.Drawing.Size(200,30)
+    $button1_exe_hellomydir.Text = "Hello-My-Dir"
+    $button1_exe_hellomydir.Add_Click({
+        Start-Process powershell -ArgumentList "-File Hello-My-Dir\Hello_My_Dir.ps1"
+        # Attendre que le premier script se termine avant d'exécuter le second script
+        Start-Sleep -Seconds 30 # Ajuste le temps d'attente si nécessaire
+        Start-Process powershell -ArgumentList "-File Hello-My-Dir\Hello_My_Dir_Config.ps1"
     })
-    $form.Controls.Add($launchButton)
+    # BUTTON 1 --> HELLO-MY-DIR
+    
+    # BUTTON 2 --> HARDEN-AD
+    $button2_exe_hardenad = New-Object System.Windows.Forms.Button
+    $button2_exe_hardenad.Location = New-Object System.Drawing.Point(50,90)
+    $button2_exe_hardenad.Size = New-Object System.Drawing.Size(200,30)
+    $button2_exe_hardenad.Text = "Execute Harden-AD"
+    $button2_exe_hardenad.Add_Click({
+        Start-Process powershell -ArgumentList "-File Harden-AD\Harden-AD_start.ps1"
+    })
+    # BUTTON 2 --> HARDEN-AD
+    
+    # BUTTON 3 --> PINGCASTLE
+    $button3_exe_pingcastle = New-Object System.Windows.Forms.Button
+    $button3_exe_pingcastle.Location = New-Object System.Drawing.Point(50,130)
+    $button3_exe_pingcastle.Size = New-Object System.Drawing.Size(200,30)
+    $button3_exe_pingcastle.Text = "Execute PingCastle"
+    $button3_exe_pingcastle.Add_Click({
+        $pingcastle_path = "C:\ODIN\PingCastle"
+        Start-Process powershell -ArgumentList "-NoProfile -NoExit -Command `"Set-Location -Path $pingcastle_path; .\PingCastle.exe`""
+    })
+    # BUTTON 3 --> PINGCASTLE
+    
+    # BUTTON 4 --> PURPLEKNIGHT
+    $button4_exe_purpleknight = New-Object System.Windows.Forms.Button
+    $button4_exe_purpleknight.Location = New-Object System.Drawing.Point(50,170)
+    $button4_exe_purpleknight.Size = New-Object System.Drawing.Size(200,30)
+    $button4_exe_purpleknight.Text = "Execute PurpleKnight"
+    $button4_exe_purpleknight.Add_Click({
+        $purpleknight_path = "C:\ODIN\PurpleKnight\PK Community 4.2"
+        Start-Process powershell -ArgumentList "-NoProfile -NoExit -Command `"Set-Location -Path $purpleknight_path; .\PurpleKnight.exe`""
+    })
+    # BUTTON 4 --> PURPLEKNIGHT
+    
+    # BUTTON 5 --> AD ACL Scanner
+    $button5_exe_adaclscanner = New-Object System.Windows.Forms.Button
+    $button5_exe_adaclscanner.Location = New-Object System.Drawing.Point(50,210)
+    $button5_exe_adaclscanner.Size = New-Object System.Drawing.Size(200,30)
+    $button5_exe_adaclscanner.Text = "Execute AD ACL Scanner"
+    $button5_exe_adaclscanner.Add_Click({
+        $adaclscanner_path = "C:\ODIN\AD ACL Scanner\ADACLScanner-master"
+        Start-Process powershell -ArgumentList "-NoProfile -NoExit -Command `"Set-Location -Path $adaclscanner_path; .\ADACLScan.ps1`""
+    })
+    # BUTTON 5 --> AD ACL Scanner
+    
+    # BUTTON 6 --> FINISH
+    $button6_finish = New-Object System.Windows.Forms.Button
+    $button6_finish.Location = New-Object System.Drawing.Point(50,250)
+    $button6_finish.Size = New-Object System.Drawing.Size(200,30)
+    $button6_finish.Text = "Finish"
+    $button6_finish.Add_Click({
+        $form.Close()
+    })
+    # BUTTON 6 --> FINISH
+
+    # Ajout des contrôles à la Forme
+    $form.Controls.Add($button1_exe_hellomydir) # --> Hello-My-Dir
+    $form.Controls.Add($button2_exe_hardenad) # --> HardenAD
+    $form.Controls.Add($button3_exe_pingcastle) # --> PingCastle
+    $form.Controls.Add($button4_exe_purpleknight) # --> PurpleKnight
+    $form.Controls.Add($button5_exe_adaclscanner) # --> AD ACL Scanner
+    $form.Controls.Add($button6_finish) # --> Finish
 
     $form.ShowDialog()
 }
