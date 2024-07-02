866d024d16313db98d3a7128fb714fc4ed867287
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-07-02 16:08:18 +0200

    Update ADACLScanner.ps1

--- ADACLScanner/ADACLScanner.ps1
+++ ADACLScanner/ADACLScanner.ps1
@@ -1,17 +1,17 @@
 # Vérifie si le dossier C:\ODIN existe, sinon le créer
 if (-Not (Test-Path -Path "C:\ODIN")) {
     New-Item -Path "C:\ODIN" -ItemType Directory
-    Write-Host "Dossier C:\ODIN créé."
+    Write-Host "Folder C:\ODIN created."
 } else {
-    Write-Host "Dossier C:\ODIN existe déjà."
+    Write-Host "Folder C:\ODIN already exist."
 }
 
 # Vérifie si le dossier C:\ODIN\Hello-My-Dir existe, sinon le créer
 if (-Not (Test-Path -Path "C:\ODIN\AD ACL Scanner")) {
     New-Item -Path "C:\ODIN\AD ACL Scanner" -ItemType Directory
-    Write-Host "Dossier C:\ODIN\AD ACL Scanner créé."
+    Write-Host "Folder C:\ODIN\AD ACL Scanner created."
 } else {
-    Write-Host "Dossier C:\ODIN\AD ACL Scanner existe déjà."
+    Write-Host "Folder C:\ODIN\AD ACL Scanner already existt."
 }
 
 # Télécharge le fichier zip depuis GitHub
@@ -19,13 +19,13 @@ $zipUrl = "https://github.com/canix1/ADACLScanner/archive/refs/heads/master.zip"
 $zipPath = "C:\ODIN\AD ACL Scanner\AD-ACL-Scanner.zip"
 
 Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
-Write-Host "Téléchargement de l'archive GitHub terminé."
+Write-Host "GitHub archive download completed."
 
 # Décompresse le fichier zip téléchargé
 Expand-Archive -Path $zipPath -DestinationPath "C:\ODIN\AD ACL Scanner"
-Write-Host "Décompression de l'archive terminée."
+Write-Host "Archive decompression completed."
 
 # Supprime le fichier zip après décompression
 Remove-Item -Path $zipPath
-Write-Host "Fichier zip supprimé."
+Write-Host "Zip file deleted."
 
